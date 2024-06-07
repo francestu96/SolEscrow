8fa325cc23ca9c55b7c773ba8e57c516367d0c14
francestu96 <francestu96@gmail.com>
2024-06-07 15:59:49 +0200

    Hero update

--- src/elements/Hero.tsx
+++ src/elements/Hero.tsx
@@ -9,12 +9,10 @@ const Hero = () => {
     shouldForwardProp: (prop) => isValidMotionProp(prop) || shouldForwardProp(prop),
   })
   return (
-    <Container maxWidth="unset" p="10" minH="88vh" display="flex" background="linear-gradient(0.1turn, #9945FF, #14F195);" backgroundSize="cover" justifyContent="center">
+    <Container maxWidth="unset" p="10" zIndex="-1" minH="88vh" display="flex" background="linear-gradient(0.1turn, #9945FF, #14F195);" backgroundSize="cover" justifyContent="center">
         <VStack width={["100%", "80%", "50%"]} justifyContent="center">
-            <Box className="animated-hero-border" w={["100%", "100%", "100%", "100%", "60%"]} display="flex" position="relative" justifyContent="center">
-                <ChakraBox zIndex={10} backgroundColor="gray.700" w="97%" mt="6px" h="95%" borderRadius="3xl" p="5" mb="5" fontFamily="Satoshi-Bold" animate={{opacity: [0, 1], filter: ["blur(12px)", "blur(0px)"] }} transition={{duration: "1.5", ease: "easeInOut", delay: "0.5"}} opacity="0">
-                    <Text textAlign="center" fontSize={["4xl", "6xl", "8xl"]} background="linear-gradient(to right, white, #5d93cf);" fill="transparent" backgroundClip="text">SolEscrow</Text>
-                </ChakraBox>
+            <Box className="animated-hero" backgroundColor="gray.700" p="5" mb="5" borderRadius="3xl" fontFamily="Satoshi-Bold" w={["100%", "100%", "100%", "100%", "60%"]} position="relative">
+                <Text textAlign="center" fontSize={["4xl", "6xl", "8xl"]} background="linear-gradient(to right, white, #5d93cf);" fill="transparent" backgroundClip="text">SolEscrow</Text>
             </Box>
             <ChakraBox animate={textAnimation} transition={transition} opacity="0" mb="10" mt={["5", "4", "2", "0"]}>
                 <Text fontSize="2xl" color="rgb(23,25,35,0.8)" textAlign="center" fontFamily="Satoshi-Bold" mb="10">&quot;Powering Decentralization, Build Trust&quot;</Text>
