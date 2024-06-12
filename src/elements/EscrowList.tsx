209c8920705eda2aa2d67fabe92c2751a834dda8
francestu96 <francestu96@gmail.com>
2024-06-12 12:01:28 +0200

    Reversed escrows list

--- src/elements/EscrowList.tsx
+++ src/elements/EscrowList.tsx
@@ -20,6 +20,8 @@ const EscrowList = ({ escrows, sent, approved, web3button, releaseButton, wallet
   const [isDenyLoadingIndex, setIsDenyLoadingIndex] = useState(-1);
   const [isReleaseLoadingIndex, setIsReleaseLoadingIndex] = useState(-1);
 
+  const lastIndex = (escrows?.amount.length || 0) - 1
+
   const approveEscrow = async (approve: boolean, index: number, escrowSender: PublicKey, escrowReceiver: PublicKey, status: Buffer) => {
     if (approve){
         setIsApproveLoadingIndex(index);
@@ -49,7 +51,7 @@ const EscrowList = ({ escrows, sent, approved, web3button, releaseButton, wallet
 
       toast({ description: "Transaction success", status: 'success', position: "bottom-right", isClosable: true, duration: 3000 });
 
-      status[index] = approve ? 1 : 2;
+      status[lastIndex - index] = approve ? 1 : 2;
 
       setIsApproveLoadingIndex(-1);
       setIsDenyLoadingIndex(-1);
@@ -87,7 +89,7 @@ const EscrowList = ({ escrows, sent, approved, web3button, releaseButton, wallet
 
       toast({ description: "Transaction success", status: 'success', position: "bottom-right", isClosable: true, duration: 3000 });
 
-      status[index] = 3;
+      status[lastIndex - index] = 3;
 
       setIsReleaseLoadingIndex(-1);
     }
@@ -113,20 +115,20 @@ const EscrowList = ({ escrows, sent, approved, web3button, releaseButton, wallet
       {!escrows ? (
         <Text fontFamily="satoshi-bold" fontSize="lg" align="center">No Escrows Found</Text>
       ) : (
-        escrows.amount.map((amount: BN, index: number) => (
-          <Card mt="5" key={index} borderWidth="thin" borderColor={escrows.status[index] === 1 ? "limegreen" : (escrows.status[index] === 2 ? "main" : "inherit")}>
+        escrows.amount.toReversed().map((amount: BN, index: number) => (
+          <Card mt="5" key={index} borderWidth="thin" borderColor={escrows.status[lastIndex - index] === 1 ? "limegreen" : (escrows.status[lastIndex - index] === 2 ? "main" : "inherit")}>
             <CardHeader py="2">
               {sent ? (
                 <Stack direction={["column", "column", "row", "row"]} justifyContent="space-between">
-                  <HStack p="2" color="limegreen" display={escrows.status[index] === 1 ? "flex" : "none"}>
+                  <HStack p="2" color="limegreen" display={escrows.status[lastIndex - index] === 1 ? "flex" : "none"}>
                     <Text fontSize={["xs", "xs", "sm", "md"]} fontWeight="bold">APPROVED</Text>
                     <FaCheck />
                   </HStack>
-                  <HStack p="2" color="main" display={escrows.status[index] === 2 ? "flex" : "none"}>
+                  <HStack p="2" color="main" display={escrows.status[lastIndex - index] === 2 ? "flex" : "none"}>
                     <Text fontSize={["xs", "xs", "sm", "md"]} fontWeight="bold">DENIED</Text>
                     <ImCross />
                   </HStack>
-                  <HStack p="2" color="orange.300" display={escrows.status[index] === 0 && amount && amount.toNumber() > 0 ? "flex" : "none"}>
+                  <HStack p="2" color="orange.300" display={escrows.status[lastIndex - index] === 0 && amount && amount.toNumber() > 0 ? "flex" : "none"}>
                     <Text fontSize={["xs", "xs", "sm", "md"]} fontWeight="bold">Waiting for approval</Text>
                     <Spinner speed="2s" ml="2" />
                   </HStack>
@@ -134,13 +136,13 @@ const EscrowList = ({ escrows, sent, approved, web3button, releaseButton, wallet
                     <HStack fontSize={["3xs", "2xs", "xs", "sm"]}>
                       <Text>To:</Text>
                       <Box backgroundColor={colorMode == "dark" ? "gray.800" : "gray.200"} rounded="lg" p="2">
-                        <Text>{escrows.receiver![index].toString()}</Text>
+                        <Text>{escrows.receiver![lastIndex - index].toString()}</Text>
                       </Box>
                     </HStack>
                     <HStack fontSize={["3xs", "2xs", "xs", "sm"]}>
                       <Text>Approver:</Text>
                       <Box backgroundColor={colorMode == "dark" ? "gray.800" : "gray.200"} rounded="lg" p="2">
-                        <Text>{escrows.approver![index].toString()}</Text>
+                        <Text>{escrows.approver![lastIndex - index].toString()}</Text>
                       </Box>
                     </HStack>
                   </VStack>
@@ -148,15 +150,15 @@ const EscrowList = ({ escrows, sent, approved, web3button, releaseButton, wallet
               ) : (
                 <Stack direction={["column", "column", "row", "row"]} justifyContent="space-between">
                   <HStack>
-                    <HStack display={web3button && escrows.status[index] === 0 ? "flex" : "none"}>
-                      <Button isLoading={isApproveLoadingIndex === index} style={{ padding: "0", minWidth: "unset", background: "transparent" }} onClick={() => approveEscrow(true, index, escrows.sender![index], escrows.receiver![index], escrows.status)}>
+                    <HStack display={web3button && escrows.status[lastIndex - index] === 0 ? "flex" : "none"}>
+                      <Button isLoading={isApproveLoadingIndex === index} style={{ padding: "0", minWidth: "unset", background: "transparent" }} onClick={() => approveEscrow(true, lastIndex - index, escrows.sender![lastIndex - index], escrows.receiver![lastIndex - index], escrows.status)}>
                         <Tooltip hasArrow label='Approve' bg='gray.300' color='black' placement="top">
                           <span>
                             <AiOutlineCheckCircle style={{ color: "lime", fontSize: "35px" }} />
                           </span>
                         </Tooltip>
                       </Button>
-                      <Button isLoading={isDenyLoadingIndex === index} style={{ padding: "0", minWidth: "unset", background: "transparent" }} onClick={() => approveEscrow(false, index, escrows.sender![index], escrows.receiver![index], escrows.status)}>
+                      <Button isLoading={isDenyLoadingIndex === index} style={{ padding: "0", minWidth: "unset", background: "transparent" }} onClick={() => approveEscrow(false, lastIndex - index, escrows.sender![lastIndex - index], escrows.receiver![lastIndex - index], escrows.status)}>
                         <Tooltip hasArrow label='Deny' bg='gray.300' color='black' placement="top">
                           <span>
                             <PiProhibitInset style={{ color: "red", fontSize: "37px" }} />
@@ -164,15 +166,15 @@ const EscrowList = ({ escrows, sent, approved, web3button, releaseButton, wallet
                         </Tooltip>
                       </Button>
                     </HStack>
-                    <HStack p="2" color="limegreen" display={escrows.status[index] === 1 ? "flex" : "none"}>
+                    <HStack p="2" color="limegreen" display={escrows.status[lastIndex - index] === 1 ? "flex" : "none"}>
                       <Text fontSize={["xs", "xs", "sm", "md"]} fontWeight="bold">APPROVED</Text>
                       <FaCheck />
                     </HStack>
-                    <HStack p="2" color="main" display={escrows.status[index] === 2 ? "flex" : "none"}>
+                    <HStack p="2" color="main" display={escrows.status[lastIndex - index] === 2 ? "flex" : "none"}>
                       <Text fontSize={["xs", "xs", "sm", "md"]} fontWeight="bold">DENIED</Text>
                       <ImCross />
                     </HStack>
-                    <HStack p="2" color="orange.300" display={escrows.status[index] === 0 && !web3button ? "flex" : "none"}>
+                    <HStack p="2" color="orange.300" display={escrows.status[lastIndex - index] === 0 && !web3button ? "flex" : "none"}>
                       <Text fontSize={["xs", "xs", "sm", "md"]} fontWeight="bold">Waiting for approval</Text>
                       <Spinner speed="2s" ml="2" />
                     </HStack>
@@ -181,7 +183,7 @@ const EscrowList = ({ escrows, sent, approved, web3button, releaseButton, wallet
                     <HStack fontSize={["3xs", "2xs", "xs", "sm"]}>
                       <Text>From:</Text>
                       <Box backgroundColor={colorMode == "dark" ? "gray.800" : "gray.200"} rounded="lg" p="2">
-                        <Text>{escrows.sender ? escrows.sender[index].toString() : wallet?.publicKey.toString()}</Text>
+                        <Text>{escrows.sender ? escrows.sender[lastIndex - index].toString() : wallet?.publicKey.toString()}</Text>
                       </Box>
                     </HStack>
                     {
@@ -189,14 +191,14 @@ const EscrowList = ({ escrows, sent, approved, web3button, releaseButton, wallet
                         <HStack fontSize={["3xs", "2xs", "xs", "sm"]}>
                           <Text>To:</Text>
                           <Box backgroundColor={colorMode == "dark" ? "gray.800" : "gray.200"} rounded="lg" p="2">
-                            <Text>{escrows.receiver ? escrows.receiver[index].toString() : wallet?.publicKey.toString()}</Text>
+                            <Text>{escrows.receiver ? escrows.receiver[lastIndex - index].toString() : wallet?.publicKey.toString()}</Text>
                           </Box>
                         </HStack>
                       ) : (
                         <HStack fontSize={["3xs", "2xs", "xs", "sm"]}>
                           <Text>Approver:</Text>
                           <Box backgroundColor={colorMode == "dark" ? "gray.800" : "gray.200"} rounded="lg" p="2">
-                            <Text>{escrows.approver ? escrows.approver[index].toString() : wallet?.publicKey.toString()}</Text>
+                            <Text>{escrows.approver ? escrows.approver[lastIndex - index].toString() : wallet?.publicKey.toString()}</Text>
                           </Box>
                         </HStack>
                       )
@@ -216,11 +218,11 @@ const EscrowList = ({ escrows, sent, approved, web3button, releaseButton, wallet
                         <Text fontSize={["xs", "sm", "md", "md"]} ><b>SOL:</b> {(amount.toNumber() / 10**9)}</Text>
                       </Box>
                     </Tooltip>
-                    <Text fontSize={["xs", "sm", "md", "md"]} whiteSpace="pre">{escrows.message[index]}</Text>
+                    <Text fontSize={["xs", "sm", "md", "md"]} whiteSpace="pre">{escrows.message[lastIndex - index]}</Text>
                   </HStack>
                 ) : (
                   <HStack justifyContent="space-between" flexWrap="wrap" overflow="auto">
-                    <Text fontSize={["xs", "sm", "md", "md"]} whiteSpace="pre">{escrows.message[index]}</Text>
+                    <Text fontSize={["xs", "sm", "md", "md"]} whiteSpace="pre">{escrows.message[lastIndex - index]}</Text>
                     <Tooltip hasArrow label='Amount with fees' bg='gray.300' color='black' placement="top">
                       <Box w={["40%", "30%", "20%", "10%"]} backgroundColor={colorMode == "dark" ? "gray.900" : "gray.100"} rounded="lg" p="2">
                         <Text fontSize={["xs", "sm", "md", "md"]} ><b>SOL:</b> {(amount.toNumber() / 10**9)}</Text>
@@ -234,27 +236,27 @@ const EscrowList = ({ escrows, sent, approved, web3button, releaseButton, wallet
               <HStack width="100%" alignItems="end">
                 <HStack fontSize={["3xs", "2xs", "xs", "xs"]} py="2" width="100%" justifyContent={"space-between"}>
                   <Text>
-                    {new Date(escrows.timestamp[index].toNumber() * 1000).toLocaleDateString(undefined, { hour: "2-digit", minute: "2-digit", hourCycle: "h24" })}
+                    {new Date(escrows.timestamp[lastIndex - index].toNumber() * 1000).toLocaleDateString(undefined, { hour: "2-digit", minute: "2-digit", hourCycle: "h24" })}
                   </Text>
                 </HStack>
-                {sent && escrows.status[index] === 0 && releaseButton &&
+                {sent && escrows.status[lastIndex - index] === 0 && releaseButton &&
                   (
                     <Button
-                      onClick={() => releaseEscrow(index, escrows.receiver![index], escrows.approver![index], escrows.status)}
-                      _disabled={{backgroundColor: checkReleaseTime(escrows.timestamp[index]) != 0 ? "#C53030" : (colorMode == "dark" ? "lawngreen" : "green"), cursor: "not-allowed"}}
-                      isDisabled={checkReleaseTime(escrows.timestamp[index]) != 0} 
+                      onClick={() => releaseEscrow(lastIndex - index, escrows.receiver![lastIndex - index], escrows.approver![lastIndex - index], escrows.status)}
+                      _disabled={{backgroundColor: checkReleaseTime(escrows.timestamp[lastIndex - index]) != 0 ? "#C53030" : (colorMode == "dark" ? "lawngreen" : "green"), cursor: "not-allowed"}}
+                      isDisabled={checkReleaseTime(escrows.timestamp[lastIndex - index]) != 0} 
                       isLoading={isReleaseLoadingIndex === index}
-                      style = {{ fontSize: "inherit", maxWidth: "50%", maxHeight: "2.5rem", color: colorMode == "dark" ? "#171923" : "white", backgroundColor: checkReleaseTime(escrows.timestamp[index]) != 0 ? "#C53030" : (colorMode == "dark" ? "lawngreen" : "green") }}
+                      style = {{ fontSize: "inherit", maxWidth: "50%", maxHeight: "2.5rem", color: colorMode == "dark" ? "#171923" : "white", backgroundColor: checkReleaseTime(escrows.timestamp[lastIndex - index]) != 0 ? "#C53030" : (colorMode == "dark" ? "lawngreen" : "green") }}
                     >
-                      <HStack justifyContent="space-between" width={checkReleaseTime(escrows.timestamp[index]) == 0 ? "80%" : "100%"} px="2">
+                      <HStack justifyContent="space-between" width={checkReleaseTime(escrows.timestamp[lastIndex - index]) == 0 ? "80%" : "100%"} px="2">
                         {
-                          checkReleaseTime(escrows.timestamp[index]) == 0
+                          checkReleaseTime(escrows.timestamp[lastIndex - index]) == 0
                             ? <Text fontSize="md">Release</Text>
                             : (
-                              checkReleaseTime(escrows.timestamp[index]) ? (
+                              checkReleaseTime(escrows.timestamp[lastIndex - index]) ? (
                                 <VStack gap="1">
                                   <Text>Release in</Text>
-                                  <Countdown date={new Date(new Date().getTime() + checkReleaseTime(escrows.timestamp[index]) * 1000)} />
+                                  <Countdown date={new Date(new Date().getTime() + checkReleaseTime(escrows.timestamp[lastIndex - index]) * 1000)} />
                                 </VStack>
                               ) : <Text>Please reload</Text>
                             )
